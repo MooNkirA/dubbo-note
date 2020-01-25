0c3769be187b6d0bbfb56cb702e7686e6aeb1216
MooNkirA <>
2020-01-25 17:34:27 +0800

    【feat】 增加远程调用时使用静态代理

--- dubbo-sample/base-rpc-consumer/src/main/java/com/moon/ConsumerApplication.java
+++ dubbo-sample/base-rpc-consumer/src/main/java/com/moon/ConsumerApplication.java
@@ -1,7 +1,9 @@
 package com.moon;
 
 import com.alibaba.fastjson.JSON;
+import com.moon.entity.OrderEntiry;
 import com.moon.service.InfoService;
+import com.moon.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -48,14 +50,48 @@ public class ConsumerApplication {
 
         /* 2. 测试远程服务反射调用实现 */
         // 封装需要网络传输的反射调用信息
-        Map<String, String> info = new HashMap<>();
+        /*Map<String, String> info = new HashMap<>();
         info.put("target", "com.moon.service.OrderService");
         info.put("methodName", "getDetail");
         info.put("arg", "1");
         // 调用传输数据方法
         Object result = infoService.passInfo(info);
-        LOGGER.info("测试远程调用功能InfoService.passInfo()，调用结果：{}", JSON.toJSONString(result));
+        LOGGER.info("测试远程调用功能InfoService.passInfo()，调用结果：{}", JSON.toJSONString(result));*/
 
+
+        /* 3. 优化改造，使用静态代理封装参数准备的过程 */
+        OrderService orderService = getService(infoService);
+        // 透明化调用，不需要调用者去关心调用的准备工作
+        OrderEntiry entiry = orderService.getDetail("1");
+        LOGGER.info("测试静态代理远程调用InfoService.passInfo()方法，调用结果：{}", JSON.toJSONString(entiry));
+    }
+
+    /**
+     * 静态代理
+     *
+     * @param infoService 网络传输类
+     * @return 返回OrderService代理示例
+     */
+    private static OrderService getService(InfoService infoService) {
+        // 返回接口实现
+        return new OrderService() {
+            @Override
+            public OrderEntiry getDetail(String id) {
+                Map<String, String> info = new HashMap<>();
+                // 因为知道反射的目标与参数，使用静态代理直接封装相应参数
+                info.put("target", "com.moon.service.OrderService"); // 调用对象
+                info.put("methodName", "getDetail"); // 调用方法
+                info.put("arg", id); // 调用参数
+
+                OrderEntiry result = null;
+                try {
+                    result = (OrderEntiry) infoService.passInfo(info);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+        };
     }
 
 }
