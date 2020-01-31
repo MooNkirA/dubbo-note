b74e1b32d1d4e9bb46d8b7972851233deaac7e57
MooNkirA <>
2020-01-31 12:32:18 +0800

    【feat】 更新java spi基础使用demo

---
+++ dubbo-thought/base-spi/src/main/java/com/moon/service/impl/info/InfoServiceCImpl.java
@@ -0,0 +1,21 @@
+package com.moon.service.impl.info;
+
+import com.alibaba.dubbo.common.URL;
+import com.moon.service.InfoService;
+
+//@Adaptive
+public class InfoServiceCImpl implements InfoService {
+
+    @Override
+    public Object sayHello(String name) {
+        System.out.println(name + ",你好，调通了C实现！");
+        return name + ",你好，调通了C实现！";
+    }
+
+    @Override
+    public Object passInfo(String msg, URL url) {
+        System.out.println("恭喜你，调通了C实现");
+        return msg;
+    }
+
+}
---
+++ dubbo-thought/busi-logic/src/main/java/com/moon/spi/SpiTest.java
@@ -0,0 +1,31 @@
+package com.moon.spi;
+
+import com.moon.service.InfoService;
+
+import java.util.ServiceLoader;
+
+/**
+ * SPI 示例测试
+ *
+ * @author MoonZero
+ * @version 1.0
+ * @date 2020-1-31 11:57
+ * @description
+ */
+public class SpiTest {
+
+    /**
+     * 测试Java SPI机制
+     */
+    @Test
+    public void testBaseSpi() {
+        //服务加载器，加载实现类
+        ServiceLoader<InfoService> serviceLoader = ServiceLoader.load(InfoService.class);
+
+        // ServiceLoader是实现了Iterable的迭代器，直接遍历实现类
+        for (InfoService service : serviceLoader) {
+            Object result = service.sayHello("Moon"); // 依次调用文件中配置的所有实现类
+        }
+    }
+
+}
