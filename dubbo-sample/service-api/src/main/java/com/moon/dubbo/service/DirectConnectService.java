9745e9c0b80e0c33013ac9a9cb06378529fd4877
MooNkirA <lje888@126.com>
2021-07-15 14:29:18 +0800

    feat: dubbo高阶应用，直连服务提供、注解配置多协议等示例

---
+++ dubbo-sample/service-api/src/main/java/com/moon/dubbo/service/DirectConnectService.java
@@ -0,0 +1,15 @@
+package com.moon.dubbo.service;
+
+/**
+ * 直连提供者测试接口
+ *
+ * @author MooNkirA
+ * @version 1.0
+ * @date 2021-07-13 19:31
+ * @description
+ */
+public interface DirectConnectService {
+
+    String queryData();
+
+}
